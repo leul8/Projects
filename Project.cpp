065f03ba02b533a254273b3a1ded1705d5a9ec83
leul8 <132384655+leul8@users.noreply.github.com>
2023-12-24 14:14:57 +0300

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
  char num[3][3] = {{'1','2','3'},
@@ -9,7 +10,50 @@ int num1;
 string welc;
 string fplay;
 string splay;
+void tictactoeboard();
+void check(int num1);
+void changeplayer();
+void checkwinner();
+int main()
+{
+    cout << "---------------------                                                                               ----------------" << endl;
+    cout << "                                      WELCOME TO TIC TAC TOE" << endl;
+    cout << "---------------------                                                                               ----------------" << endl;
+    cout << "                                       TYPE START TO BEGIN" << endl;
+    cin >> welc;
 
+    if (welc == "START")
+    {
+        cout << "PLEASE ENTER THE NAME OF THE FIRST PLAYER:"; \
+            cin >> fplay;
+        cout << "PLEASE ENTER THE NAME OF THE SECOND PLAYER:"; \
+            cin >> splay;
+        tictactoeboard();
+    }
+    else
+    {
+        cout << "TRY AGAIN!";
+    }
+
+    while (1)
+    {
+        cout << fplay << ":";
+        cin >> num1;
+        check(num1);
+        tictactoeboard();
+        changeplayer();
+        checkwinner();
+        cout << splay << ":";
+        cin >> num1;
+        check(num1);
+        tictactoeboard();
+        changeplayer();
+        checkwinner();
+    }
+
+
+
+}
 void tictactoeboard()
 {
     /* for (int i = 0; i < 3; i++)
@@ -25,33 +69,11 @@ void tictactoeboard()
     cout << "                                          | " << num[2][0] << " |  " << num[2][1] << " | " << num[2][2] << " |" << endl;
 
 }
-void welcome()
-{
-    cout << "---------------------                                                                               ----------------" << endl;
-    cout << "                                      WELCOME TO TIC TAC TOE" << endl;
-    cout << "---------------------                                                                               ----------------" << endl;
-    cout << "                                       TYPE START TO BEGIN" << endl;
-    cin >> welc;
-    
-    if (welc == "START")
-    {
-        cout << "PLEASE ENTER THE NAME OF THE FIRST PLAYER:"; \
-            cin >> fplay;
-        cout << "PLEASE ENTER THE NAME OF THE SECOND PLAYER:"; \
-            cin >> splay;
-        tictactoeboard();
-    }
-    else
-    {
-        cout << "TRY AGAIN!";
-    }
-    
-}
 
 
-void check()
+
+void check(int num1)
 {
-    cin >> num1;
     switch (num1)
     {
     case 1:
@@ -71,10 +93,13 @@ void check()
         break;
     case 6:
         num[1][2] = xo;
+        break;
     case 7:
         num[2][0] = xo;
+        break;
     case 8:
         num[2][1] = xo;
+        break;
     case 9:
         num[2][2] = xo;
         break;
@@ -90,24 +115,34 @@ void changeplayer()
     {
         xo = 'X';
     }
-}
 
-int main()
-{
-    welcome();
-    while (1)
-    {
-     cout << fplay << ":";
-     check();
-     tictactoeboard();
-     changeplayer();
-     cout << splay << ":";
-     check();
-     tictactoeboard();
-     changeplayer();
-    }
-    
    
-
 }
 
+ void checkwinner()
+    {
+     static int i = 0;
+     string count;
+     fstream file4;
+     file4.open("count.txt", ios::out);
+     if (num[0][0] == 'X' && num[1][1] == 'X' && num[2][2] == 'X')
+     {
+         i = i + 1;
+         file4 << i;
+         cout << fplay << " has won!";
+
+     }
+     file4.close();
+     file4.open("count.txt", ios::in);
+     file4 >> count;
+     if (count == "2")
+     {
+         cout << fplay << " has won!";
+         exit(1);
+     }
+     
+    }
+/*cout << endl;
+cout << "                                          | " << num[0][0] << " |  " << num[0][1] << " | " << num[0][2] << " |" << endl;
+cout << "                                          | " << num[1][0] << " |  " << num[1][1] << " | " << num[1][2] << " |" << endl;
+cout << "                                          | " << num[2][0] << " |  " << num[2][1] << " | " << num[2][2] << " |" << endl;*/
