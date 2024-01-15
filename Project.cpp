926de15824ee8732885c0722fd629dc08c3ab512
leul8 <132384655+leul8@users.noreply.github.com>
2024-01-15 21:01:39 +0300

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -1,71 +1,65 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
- char num[3][3] = {{'1','2','3'},
-                  {'4','5','6'},
-                  {'7','8','9'}};
+fstream file4;
+char num[3][3] =  {{'1','2','3'},
+                   {'4','5','6'},
+                   {'7','8','9'}};
 char xo = 'X';
-int num1;
-string welc;
-string fplay;
-string splay;
+int Count;
 void tictactoeboard();
 void check(int num1);
 void changeplayer();
-void checkwinner();
+void checkwinner(string *p);
+void resetboard();
 int main()
 {
+    string welc;
+    int num1;
+    string player[2];
+    file4.open("Board.txt", ios::out);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            file4 << num[i][j];
+        }
+    }
+    file4.close();
     cout << "---------------------                                                                               ----------------" << endl;
     cout << "                                      WELCOME TO TIC TAC TOE" << endl;
     cout << "---------------------                                                                               ----------------" << endl;
-    cout << "                                       TYPE START TO BEGIN" << endl;
-    cin >> welc;
-
-    if (welc == "START")
-    {
-        cout << "PLEASE ENTER THE NAME OF THE FIRST PLAYER:"; \
-            cin >> fplay;
-        cout << "PLEASE ENTER THE NAME OF THE SECOND PLAYER:"; \
-            cin >> splay;
-        tictactoeboard();
-    }
-    else
-    {
-        cout << "TRY AGAIN!";
-    }
-
+        cout << "PLEASE ENTER THE NAME OF THE FIRST PLAYER:"; 
+            cin >> player[0];
+        cout << "PLEASE ENTER THE NAME OF THE SECOND PLAYER:"; 
+            cin >> player[1];
+            tictactoeboard();
     while (1)
     {
-        cout << fplay << ":";
+        cout << player[0] << ":";
         cin >> num1;
+        Count++;
         check(num1);
         tictactoeboard();
         changeplayer();
-        checkwinner();
-        cout << splay << ":";
+        checkwinner(player);
+        cout << player[1] << ":";
         cin >> num1;
+        Count++;
         check(num1);
         tictactoeboard();
         changeplayer();
-        checkwinner();
+        checkwinner(player);
     }
 }
 void tictactoeboard()
 {
-    /* for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << "                                          | " << num[i][j] << " |  " << num[i][j] << " | " << num[i][j] << " |" << endl;
-        }
-    }*/
     cout << endl;
     cout << "                                          | " << num[0][0] << " |  " << num[0][1] << " | " << num[0][2] << " |" << endl;
     cout << "                                          | " << num[1][0] << " |  " << num[1][1] << " | " << num[1][2] << " |" << endl;
     cout << "                                          | " << num[2][0] << " |  " << num[2][1] << " | " << num[2][2] << " |" << endl;
-
 }
+
 void check(int num1)
 {
     switch (num1)
@@ -110,31 +104,68 @@ void changeplayer()
         xo = 'X';
     }
 }
-
- void checkwinner()
+ void checkwinner(string *p)
     {
-     static int i = 0;
-     string count;
-     fstream file4;
-     file4.open("count.txt", ios::out);
-     if (num[0][0] == 'X' && num[1][1] == 'X' && num[2][2] == 'X')
+     char answ;
+     if (num[0][0] + num[0][1] + num[0][2] == 264 || num[1][0] + num[1][1] + num[1][2] == 264 || num[2][0] + num[2][1] + num[2][2] == 264 ||
+         num[0][0] + num[1][0] + num[2][0] == 264 || num[0][1] + num[1][1] + num[2][1] == 264 || num[0][2] + num[1][2] + num[2][2] == 264 ||
+         num[0][0] + num[1][1] + num[2][2] == 264 || num[0][2] + num[1][1] + num[2][0] == 264)
      {
-         i = i + 1;
-         file4 << i;
-         cout << fplay << " has won!";
-
+         cout << p[0] << " has won!" << endl;
+         cout << "Play again?(Y/N)";
+         cin >> answ;
+         if (answ == 'Y')
+         {
+             resetboard();
+             tictactoeboard();
+         }
+         else
+         {
+             exit(1);
+         }
      }
-     file4.close();
-     file4.open("count.txt", ios::in);
-     file4 >> count;
-     if (count == "2")
+     if (num[0][0] + num[0][1] + num[0][2] == 237 || num[1][0] + num[1][1] + num[1][2] == 237 || num[2][0] + num[2][1] + num[2][2] == 237 ||
+         num[0][0] + num[1][0] + num[2][0] == 237 || num[0][1] + num[1][1] + num[2][1] == 237 || num[0][2] + num[1][2] + num[2][2] == 237 ||
+         num[0][0] + num[1][1] + num[2][2] == 237 || num[0][2] + num[1][1] + num[2][0] == 237)
      {
-         cout << fplay << " has won!";
-         exit(1);
+         cout << p[1] << " has won!" << endl;
+         cout << "Play again?(Y/N)";
+         cin >> answ;
+         if (answ == 'Y')
+         {
+             resetboard();
+             tictactoeboard();
+         }
+         else
+         {
+             exit(1);
+         }
+     }
+     if (Count == 9)
+     {
+         cout << "tie!" << endl;
+         cout << "Play again?(Y/N)";
+         cin >> answ;
+         if (answ == 'Y')
+         {
+             resetboard();
+             tictactoeboard();
+         }
+         else
+         {
+             exit(1);
+         }
      }
     }
-
-/*cout << endl;
-cout << "                                          | " << num[0][0] << " |  " << num[0][1] << " | " << num[0][2] << " |" << endl;
-cout << "                                          | " << num[1][0] << " |  " << num[1][1] << " | " << num[1][2] << " |" << endl;
-cout << "                                          | " << num[2][0] << " |  " << num[2][1] << " | " << num[2][2] << " |" << endl;*/
+ void resetboard()
+ {
+     file4.open("Board.txt", ios::in);
+     for (int i = 0; i < 3; i++)
+     {
+         for (int j = 0; j < 3; j++)
+         {
+             file4 >> num[i][j];
+         }
+     }
+     file4.close();
+ }
